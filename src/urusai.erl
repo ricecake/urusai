fe5434f2f3f425367b19f6a32a58316580cc8496
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2015-01-21 18:55:10 -0500

    make the startup func less verbose

--- src/urusai.erl
+++ src/urusai.erl
@@ -3,31 +3,7 @@
 -export ([start/0, stop/0]).
 
 start() ->
-    lager:start(),
-    ok = application:start(pooler),
-    ok = application:start(crypto),
-    ok = application:start(asn1),
-    ok = application:start(public_key),
-    ok = application:start(sasl),
-    ok = application:start(ssl),
-    ok = application:start(exmpp),
-    case application:start(urusai) of
-        ok ->
-            application:start(ranch),
-            application:start(cowboy),
-            urusai_http:start();
-        {error, Reason} ->
-            lager:error("Failed to start urusai: ~p", [Reason]),
-            stop()
-    end.
+	application:ensure_all_started(urusai),
+	urusai_http:start().
 
-stop() ->
-    application:stop(cowboy),
-    application:stop(ranch),
-    application:stop(urusai),
-    application:stop(exmpp),
-    application:stop(ssl),
-    application:stop(sasl),
-    application:stop(public_key),
-    application:stop(crypto),
-    application:stop(pooler).
+stop() -> application:stop(urusai).
